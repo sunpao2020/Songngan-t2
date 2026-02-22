0fac6c04f7434ca9e428a912fb4aa92f5e0eaa4e
sunpao2020 <sunpao2040@gmail.com>
2026-02-22 23:14:22 +0700

    Add files via upload

---
+++ กิจกรรม4.js
@@ -0,0 +1,24 @@
+let a = 100;
+let b = 2.5;
+let c = "A";
+let d = "B";
+let e = "C5";
+let f = "---------------------------------------"
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(a, b);
+console.log(c, d);
+console.log(e);
+console.log(f);
+console.log(`${a}${b}  ${c}${d}  ${e}`);
+console.log(f);
+console.log(a + "Hello");
+console.log(b + "Hello");
+console.log("Hello" + c);
+console.log(f);
+console.log(a + "Hello" + b + "Hello" + c);
